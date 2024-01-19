0dbc4de70b7c96473b7b4dc1356c20d56b4c3f2a
arro38 <etienne.vaytilingom.pro@gmail.com>
2024-01-19 10:44:21 +0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <button className="btn btn-success text-white hover:scale-110">
+        Success
+      </button>
+    </>
+  );
+}
+
+export default App;
